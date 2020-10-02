0258d6cbd92300cc1cdeea69d040578a22d6ecee
Davide Francavilla <davfrancavilla@gmail.com>
2020-10-02 17:41:42 +0200

    Aggiunte funzioni get e set alla classe Product

--- classes/Product.php
+++ classes/Product.php
@@ -2,10 +2,10 @@
 
 
 class Product {
-    public $price;
-    public $shelf;
-    public $quantity;
-    public $availability;
+    protected $price;
+    protected $shelf;
+    protected $quantity;
+    protected $availability;
 
 
     public function __construct($_price, $_shelf, $_quantity, $_availability = false){
@@ -13,6 +13,49 @@ class Product {
         $this->shelf = $_shelf;
         $this->quantity = $_quantity;
         $this->availability = $_availability;
+    }
+
+    // set
+    public function setPrice($_price){
+        $this->price = $_price;
+    }
+
+    public function setShelf($_shelf){
+        $this->shelf = $_shelf;
+    }
+
+    public function setQuantity($_quantity){
+        $this->quantity = $_quantity;
+    }
+
+    public function setAvailability($_availability){
+        $this->availability = $_availability;
+    }
+
+    // get
+    public function getPrice(){
+        if(empty($this->price)){
+            die("Prezzo non inserito");
+        }
+        return $this->price;
+    }
+
+    public function getShelf(){
+        if(empty($this->shelf)){
+            die("Scaffale non inserito");
+        }
+        return $this->shelf;
+    }
+
+    public function getQuantity(){
+        if(empty($this->quantity)){
+            die("Quantità non inserità");
+        }
+        return $this->quantity;
+    }
+
+    public function getAvailability(){
+        return $this->availability;
     }
 
 
--- index.php
+++ index.php
@@ -4,7 +4,14 @@ require_once "classes/Product.php";
 require_once "classes/Computer.php";
 
 
-$appleComputer = new Computer("500€", "50A", "250", true,"Laptop", "1TB", "256GB", "16GB", "16\"");
-
-
+$appleComputer = new Computer("500€", "50A", 250, true,"Laptop", "1TB", "256GB", "16GB", "16\"");
+var_dump($appleComputer);
+$appleComputer->setPrice("800€");
+$appleComputer->setShelf("34E");
+$appleComputer->setQuantity(100);
+$appleComputer->setAvailability(false);
+$appleComputer->getPrice();
+$appleComputer->getShelf();
+$appleComputer->getQuantity();
+$appleComputer->getAvailability();
 var_dump($appleComputer);
